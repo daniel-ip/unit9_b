35643862a86621944c5d6fcb21b6da9cdb817f56
daniel-ip <daniel-ip86@hotmail.com>
2015-05-17 20:48:55 +0200

    Terminado

--- unit9_2/src/unit9_2/AlumnoTest.java
+++ unit9_2/src/unit9_2/AlumnoTest.java
@@ -3,7 +3,6 @@ package unit9_2;
 
 import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 public class AlumnoTest {
 
@@ -12,18 +11,10 @@ public class AlumnoTest {
 	/*************************/
 	
 	@Test
-	public void testValidaNif1_1() {
+	public void testValidaNif1() {
 		//Primera condición: si no mide 9 caracteres
 		Alumno a = new Alumno();
 		String dni = "1234";
-		Assert.assertFalse(a.validaNif(dni));		
-	}
-	
-	@Test
-	public void testValidaNif1_2() {
-		//Primera condición: si no tiene valor
-		Alumno a = new Alumno();
-		String dni = "";
 		Assert.assertFalse(a.validaNif(dni));		
 	}
 	
@@ -64,7 +55,7 @@ public class AlumnoTest {
 	/***********************************/
 	
 	@Test
-	public void testCalculaTasaMatricula(){
+	public void testCalculaTasaMatricula1(){
 		//Si la edad no es menor de 25 ni mayor de 50 y no es familia numerosa
 		//Devuelve la tasa inicial
 		Alumno a = new Alumno();
@@ -75,8 +66,83 @@ public class AlumnoTest {
 		float tasaDevuelta = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
 		float tasaEsperada = 500.00f;
 		
-		Assert.assertEquals(tasaEsperada, tasaDevuelta);
+		Assert.assertEquals(tasaEsperada, tasaDevuelta, 0.0f);
 	}
+	
+	@Test
+	public void testCalculaTasaMatricula2(){
+		//El primer IF es cierto
+		//Tasa esperada 20000.00f
+		Alumno a = new Alumno();
+		int edad = 20;
+		boolean familiaNumerosa = false;
+		boolean repetidor = true;
+		
+		float tasaDevuelta = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
+		float tasaEsperada = 2000.00f;
+		
+		Assert.assertEquals(tasaEsperada, tasaDevuelta, 0.00f);
+	}
+	
 
+	@Test
+	public void testCalculaTasaMatricula3(){
+		//Sólo la primera condición del primer if es cierta y es familia numerosa
+		//Tasa esperada 250.00f
+		Alumno a = new Alumno();
+		int edad = 20;
+		boolean familiaNumerosa = true;
+		boolean repetidor = false;
+		
+		float tasaDevuelta = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
+		float tasaEsperada = 250.00f;
+		
+		Assert.assertEquals(tasaEsperada, tasaDevuelta, 0.00f);
+	}
+	@Test
+	public void testCalculaTasaMatricula4(){
+		//La segunda condición dell primer if dentro del else es cierto
+		//Tasa esperada 250.00f
+		Alumno a = new Alumno();
+		int edad = 66;
+		boolean familiaNumerosa = false;
+		boolean repetidor = true;
+		
+		float tasaDevuelta = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
+		float tasaEsperada = 250.00f;
+		
+		Assert.assertEquals(tasaEsperada, tasaDevuelta, 0.00f);
+	}
+	
+	@Test
+	public void testCalculaTasaMatricula4_1(){
+		//El segundo if dentro del else es cierto, pero no es familia numerosa
+		//Tasa esperada 400.00f
+		Alumno a = new Alumno();
+		int edad = 55;
+		boolean familiaNumerosa = false;
+		boolean repetidor = true;
+		
+		float tasaDevuelta = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
+		float tasaEsperada = 400.00f;
+		
+		Assert.assertEquals(tasaEsperada, tasaDevuelta, 0.00f);
+	}
+	
+	@Test
+	public void testCalculaTasaMatricula4_2(){
+		//El segundo if dentro del else es cierto y es familia numerosa
+		//Tasa esperada 150.00f
+		Alumno a = new Alumno();
+		int edad = 55;
+		boolean familiaNumerosa = true;
+		boolean repetidor = true;
+		
+		float tasaDevuelta = a.calculaTasaMatricula(edad, familiaNumerosa, repetidor);
+		float tasaEsperada = 150.00f;
+		
+		Assert.assertEquals(tasaEsperada, tasaDevuelta, 0.00f);
+	}
+	
 
 }
